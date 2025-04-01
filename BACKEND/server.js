7710eb6f4a89e0e52a622f7f93be912cb579d889
DipalRana21 <rdipal993@gmail.com>
2025-04-01 21:10:32 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -17,9 +17,18 @@ const PORT=process.env.PORT || 5000;
 
 app.use(cors({
     // origin: "http://localhost:5173", // Allow requests from your frontend
-    origin: ["https://job-portal-three-mauve.vercel.app/"],
+    origin: ["https://job-portal-three-mauve.vercel.app"],
     credentials: true, // Allow cookies if you're using authentication
 }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://job-portal-dipal-ranas-projects.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
