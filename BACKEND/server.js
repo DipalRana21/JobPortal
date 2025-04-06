98bb1e30d9edb815a3b1794521fba70984a45c31
DipalRana21 <rdipal993@gmail.com>
2025-04-06 18:26:20 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -25,13 +25,13 @@ app.use(cors({
 }));
 app.options("*", cors());
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://job-portal-three-mauve.vercel.app");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://job-portal-three-mauve.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 
 app.use(express.json());
