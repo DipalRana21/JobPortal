a39f187f99ee09c2dc250e1cbcf058fd2741adf1
DipalRana21 <rdipal993@gmail.com>
2025-04-06 21:18:57 +0530

    Update user.controller.js

--- BACKEND/controllers/user.controller.js
+++ BACKEND/controllers/user.controller.js
@@ -102,6 +102,8 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
 
+
+
         user = {
             _id: user._id,
             fullname: user.fullname,
@@ -111,7 +113,7 @@ export const login = async (req, res) => {
             profile: user.profile
 
         }
-        return res.status(200).cookie('token', token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json(
+        return res.status(200).cookie('token', token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true,  sameSite: 'None' }).json(
             {
                 message: `Welcome back ${user.fullname} `,
                 user,
