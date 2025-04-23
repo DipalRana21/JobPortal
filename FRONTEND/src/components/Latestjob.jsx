cf3cccde4fd1c97529163d6c94132b61e3d68458
DipalRana21 <rdipal993@gmail.com>
2025-04-23 15:12:22 +0530

    Update Latestjob.jsx

--- FRONTEND/src/components/Latestjob.jsx
+++ FRONTEND/src/components/Latestjob.jsx
@@ -1,15 +1,19 @@
 import react from "react";
 import Jobcards from "./jobcards";
 import './style.css';
-const randomjobs=[1,2,3,4,5,6,7,8];
+import { useSelector } from 'react-redux'; 
+
+//const randomjobs=[1,2,3,4,5,6,7,8];
+
+const {allJobs} = useSelector(store=>store.job);
 const Latestjob = () => {
 return(
     <div className="latestjob home-img">
         <h1><span style={{color:"purple"}}>Latest & Top</span> Job openings</h1>
             <div className="jobcards-content" >
                 {
-            randomjobs.slice(0,6).map((job,index)=><Jobcards  key={job._id} job={job}/>
-            )
+                    allJobs.length <= 0 ? <span>No Job Available</span> : allJobs?.slice(0,6).map((job) => <Jobcards key={job._id} job={job}/>)                       
+            
         }
             </div>
             
